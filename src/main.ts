b9bec2abbfd5e1bce7d77dba83dc7d8d7f997959
Vighnesh SL <vighnesh55642@gmail.com>
2025-11-03 10:23:33 +0530

    add dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import 'dotenv/config';
-import { join } from 'path';
-import  cookieParser from 'cookie-parser'
-import { NestExpressApplication } from '@nestjs/platform-express';
+import * as dotenv from 'dotenv';
 
+import { join } from 'path';
+import cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.use(cookieParser())
-
+  app.use(cookieParser());
+  dotenv.config();
   app.enableCors({
     origin: process.env.FRONTEND_URL,
-    credentials: true
-  })
+    credentials: true,
+  });
 
-   const isProduction = process.env.NODE_ENV === 'production';
+  const isProduction = process.env.NODE_ENV === 'production';
 
-    app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads/',
   });
- await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
-console.log(`🚀 Server running in ${isProduction ? 'production' : 'development'} mode`);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  console.log(
+    `🚀 Server running in ${isProduction ? 'production' : 'development'} mode`,
+  );
   console.log(`Server is running on port ${process.env.PORT ?? 3000}`);
 }
 bootstrap();
